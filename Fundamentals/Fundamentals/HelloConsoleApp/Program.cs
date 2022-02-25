270e03261cc684e41a8be13d5442580752bdf422
Andrei Shcharbianok <Andrei.Shcharbianok@thomsonreuters.com>
2022-02-25 18:30:10 +0300

    .NET Fundamentals Task 1 completed.

---
+++ Fundamentals/Fundamentals/HelloConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloConsoleApp;
+
+class Hello
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2 || args[0] != "-username")
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        var username = args[1];
+        Console.WriteLine($"Hello {username}");
+    }
+}
---
+++ Fundamentals/Fundamentals/HelloWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace HelloWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Done_Click(object sender, RoutedEventArgs e)
+        {
+            var username = UserName.Text;
+
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                MessageBox.Show("Username value should contain at least one character!");
+                return;
+            }
+
+            MessageBox.Show($"Hello {username}");
+        }
+    }
+}
