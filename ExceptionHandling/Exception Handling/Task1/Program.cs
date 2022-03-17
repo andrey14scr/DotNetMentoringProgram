860744c8d737d3f3e4cfefc4666043a9fc694dac
Andrei Shcharbianok <Andrei.Shcharbianok@thomsonreuters.com>
2022-03-17 22:35:17 +0300

    Exception Handling. Task 1 completed.

--- ExceptionHandling/Exception Handling/Task1/Program.cs
+++ ExceptionHandling/Exception Handling/Task1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            var input = string.Empty;
+
+            while (input != "exit")
+            {
+                Console.Write("Enter your string: ");
+                input = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine($"First character is \'{input[0]}\'");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Input string was empty!");
+                }
+                catch
+                {
+                    Console.WriteLine("Unknown error.");
+                }
+            }
         }
     }
 }
