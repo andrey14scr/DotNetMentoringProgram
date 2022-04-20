a0b1417b6068dc70b6cc2c8a806c503b382f3152
Andrei Shcharbianok <Andrei.Shcharbianok@thomsonreuters.com>
2022-04-20 11:48:07 +0300

    Unit Testing. Leap Year logic is implemented

--- UnitTesting/UnitTesting/LeapYear/YearsService.cs
+++ UnitTesting/UnitTesting/LeapYear/YearsService.cs
@@ -4,6 +4,14 @@ public class YearsService : IYearsService
 {
     public YearsType GetYearsType(int year)
     {
-        throw new NotImplementedException();
+        if (year < 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            return YearsType.Leap;
+
+        return YearsType.Common;
     }
 }
