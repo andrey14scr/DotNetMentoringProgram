ace379cfd620b4e1720ca38d0601bc1b1ff6a53a
Andrei Shcharbianok <Andrei.Shcharbianok@thomsonreuters.com>
2022-04-20 11:31:51 +0300

    Unit Testing. Harry Potter logic is implemented

--- UnitTesting/UnitTesting/HarryPotter/BooksService.cs
+++ UnitTesting/UnitTesting/HarryPotter/BooksService.cs
@@ -6,6 +6,64 @@ public class BooksService : IBooksService
 
     public float GetCost(int[] books)
     {
-        throw new NotImplementedException();
+        if (books is null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        if (books.Length != 5)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if (books.Any(x => x < 0))
+        {
+            throw new ArgumentException();
+        }
+
+        var result = 0f;
+
+        if (books.All(x => x == 0))
+        {
+            return result;
+        }
+
+        var min = books.Where(x => x != 0).Min();
+        int count;
+        float payment;
+
+        while (min > 0)
+        {
+            count = books.Count(x => x >= min);
+            payment = min * count * Price;
+
+            switch (count)
+            {
+                case 2:
+                    payment *= 0.95f;
+                    break;
+                case 3:
+                    payment *= 0.9f;
+                    break;
+                case 4:
+                    payment *= 0.8f;
+                    break;
+                case 5:
+                    payment *= 0.75f;
+                    break;
+            }
+
+            result += payment;
+
+            books = books.Select(x => x >= min ? x - min : 0).ToArray();
+
+            if (books.All(x => x == 0))
+            {
+                break;
+            }
+            min = books.Where(x => x != 0).Min();
+        }
+
+        return result;
     }
 }
