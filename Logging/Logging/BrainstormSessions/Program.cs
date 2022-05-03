3dc90476ed521649108f24cac58cb07bd13efefe
Andrei Shcharbianok <Andrei.Shcharbianok@thomsonreuters.com>
2022-05-03 13:27:36 +0300

    Logging. Task 2 completed.

--- Logging/Logging/BrainstormSessions/Program.cs
+++ Logging/Logging/BrainstormSessions/Program.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.Email;
 
 namespace BrainstormSessions
 {
@@ -9,10 +15,26 @@ namespace BrainstormSessions
     {
         public static void Main(string[] args)
         {
+            var smtpInfo = JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText("smtp-info.json"));
+
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .Enrich.FromLogContext()
                 .WriteTo.File("Logs\\logs.log")
+                .WriteTo.Email(new EmailConnectionInfo
+                {
+                    FromEmail = smtpInfo["username"],
+                    ToEmail = smtpInfo["to"],
+                    MailServer = smtpInfo["server"],
+                    NetworkCredentials = new NetworkCredential
+                    {
+                        UserName = smtpInfo["username"],
+                        Password = smtpInfo["password"]
+                    },
+                    EnableSsl = true,
+                    Port = Int32.Parse(smtpInfo["port"]),
+                    EmailSubject = smtpInfo["subject"]
+                })
                 .CreateLogger();
 
             Log.Information("Starting web host");
